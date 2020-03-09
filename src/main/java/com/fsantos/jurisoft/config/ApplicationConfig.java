6092cb716392cc0827a3abfc02b07a982575ad37
Flávio Santos <flaviosantos.dev@gmail.com>
2020-03-09 10:48:57 -0300

    Update da entidade pessoa acontece via modelMapper

--- src/main/java/com/fsantos/jurisoft/config/ApplicationConfig.java
+++ src/main/java/com/fsantos/jurisoft/config/ApplicationConfig.java
@@ -1,12 +1,17 @@
 package com.fsantos.jurisoft.config;
 
+import com.fsantos.jurisoft.model.pessoa.Pessoa;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ApplicationConfig {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
+        modelMapper.createTypeMap(Pessoa.class, Pessoa.class)
+                .addMappings(m -> m.skip(Pessoa::setId));
         return modelMapper;
     }
 }
--- src/main/java/com/fsantos/jurisoft/service/pessoa/PessoaService.java
+++ src/main/java/com/fsantos/jurisoft/service/pessoa/PessoaService.java
@@ -3,6 +3,7 @@ package com.fsantos.jurisoft.service.pessoa;
 import com.fsantos.jurisoft.exception.ResourceNotFoundException;
 import com.fsantos.jurisoft.model.pessoa.Pessoa;
 import com.fsantos.jurisoft.repository.pessoa.PessoaRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,12 +16,15 @@ import java.util.UUID;
 public class PessoaService {
 
     @Autowired
-    private PessoaRepository _pessoaRepository;
+    private PessoaRepository pessoaRepository;
 
-    public Page<Pessoa> get(Pageable page){ return _pessoaRepository.findAll(page); }
+    @Autowired
+    private ModelMapper mapper;
+
+    public Page<Pessoa> get(Pageable page){ return pessoaRepository.findAll(page); }
 
     public Pessoa get(UUID id) throws ResourceNotFoundException {
-        Optional<Pessoa> optionalPessoa = _pessoaRepository.findById(id);
+        Optional<Pessoa> optionalPessoa = pessoaRepository.findById(id);
         if (!optionalPessoa.isPresent()) {
             throw new ResourceNotFoundException("Recurso " + id + " não encontrado");
         }
@@ -28,40 +32,24 @@ public class PessoaService {
     }
 
     public Pessoa save(Pessoa pessoa){
-        Pessoa salva = _pessoaRepository.save(pessoa);
+        Pessoa salva = pessoaRepository.save(pessoa);
         return salva;
     }
 
     public Pessoa update(UUID pessoaId, Pessoa pessoa) throws ResourceNotFoundException {
-        Optional<Pessoa> optionalPessoa = _pessoaRepository.findById(pessoaId);
-        if(!optionalPessoa.isPresent()){
-            throw new ResourceNotFoundException("Recurso " +  pessoaId + " a ser editado não encontrado");
-        }
-        Pessoa p = optionalPessoa.get();
-        p.setNome(pessoa.getNome());
-        p.setCpf(pessoa.getCpf());
-        p.setRg(pessoa.getRg());
-        p.setCarteiraDeTrabalho(pessoa.getCarteiraDeTrabalho());
-        p.setPis(pessoa.getPis());
-        p.setProfissao(pessoa.getProfissao());
-        p.setDataDeNascimento(pessoa.getDataDeNascimento());
-        p.setSexo(p.getSexo());
-        return _pessoaRepository.save(p);
-
+            Pessoa p = get(pessoaId);
+            this.mapper.map(pessoa, p);
+            return this.pessoaRepository.save(p);
     }
 
     public void delete(UUID id) throws ResourceNotFoundException {
-        Optional<Pessoa> pessoa = _pessoaRepository.findById(id);
-        if(!pessoa.isPresent()){
-            throw new ResourceNotFoundException("Recurso " + id + " a ser deletado não encontrado");
-        }
-        Pessoa p = pessoa.get();
+        Pessoa p = get(id);
         p.setDeleted(true);
-        _pessoaRepository.save(p);
+        pessoaRepository.save(p);
     }
 
     public boolean existsById(UUID pessoaId) throws ResourceNotFoundException {
-        if (!_pessoaRepository.existsById(pessoaId)){
+        if (!pessoaRepository.existsById(pessoaId)){
             throw new ResourceNotFoundException();
         }
         return true;
